b8fb58c98b6d530075e80c2fabaa5e203443d555
ShehanHD <shehanhd@gmail.com>
2026-05-06 01:21:30 +0200

    fix: throw explicit error when MONGODB_URI is unset

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,7 @@
 import { MongoClient, Db } from 'mongodb'
 
-const uri = process.env.MONGODB_URI!
+const uri = process.env.MONGODB_URI
+if (!uri) throw new Error('MONGODB_URI environment variable is not set')
 const dbName = process.env.MONGODB_DB ?? 'pokemon'
 
 declare global {
